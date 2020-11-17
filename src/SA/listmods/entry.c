e2b7a9acc5dfcb49c68dc65d6e9998b09d91e580
Christopher Paschen <christopher.paschen@trustedsec.com>
2020-11-17 15:46:04 -0600

    review of listmods complete
    cleaned up some unused variables

--- src/SA/listmods/entry.c
+++ src/SA/listmods/entry.c
@@ -6,9 +6,8 @@
 
 // Inspired from: https://www.codeguru.com/cpp/w-p/win32/versioning/article.php/c4539/Versioning-in-Windows.htm
 int PrintSingleModule(char* szFile){
-    DWORD dwLen, dwUseless;
-    LPTSTR lpVI;
-    char* companyName;
+    DWORD dwLen = 0, dwUseless = 0;
+    LPSTR lpVI = NULL;
 
     dwLen = VERSION$GetFileVersionInfoSizeA((LPTSTR)szFile, &dwUseless);
     if (dwLen==0){
@@ -18,16 +17,13 @@ int PrintSingleModule(char* szFile){
 
     lpVI = (LPTSTR) KERNEL32$GlobalAlloc(GPTR, dwLen);
     if (lpVI) {
-        DWORD dwBufSize;
-        VS_FIXEDFILEINFO* lpFFI;
-        BOOL bRet = FALSE;
-        WORD* langInfo;
-        UINT cbLang;
+        WORD* langInfo = NULL;
+        UINT cbLang = 0;
         char szVerDescription[256];
         char szVerCompanyName[256];
-        LPVOID lpDescription;
-        LPVOID lpCompanyName;
-        UINT cbBufSize;
+        LPVOID lpDescription = NULL;
+        LPVOID lpCompanyName = NULL;
+        UINT cbBufSize = 0;
 
         VERSION$GetFileVersionInfoA((LPTSTR)szFile, 0, dwLen, lpVI);
 
@@ -63,7 +59,7 @@ int PrintModules(DWORD processID)
     char szModName[MAX_PATH];
 
     // Print the process identifier. (debug)
-    internal_printf("Printing modules of process ID: %u\n", processID);
+    internal_printf("Printing modules of process ID: %lu\n", processID);
 
     // Get a handle to the process.
     hProcess = KERNEL32$OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
@@ -100,6 +96,7 @@ int PrintModules(DWORD processID)
     return 0;
 }
 
+#ifdef BOF
 // TODO: Add an argument to exclude Microsoft DLLs.
 void go(char * args, int length) {
 	  datap parser;
@@ -121,7 +118,13 @@ void go(char * args, int length) {
     PrintModules(pid);
 
     printoutput(TRUE);
-    bofstop();
 
     return;
 }
+#else
+
+int main()
+{
+    PrintModules(KERNEL32$GetCurrentProcessId());
+}
+#endif
