e75226b6c0b2632a1437c21a7684456faf2beb67
Christopher Paschen <christopher.paschen@trustedsec.com>
2021-03-25 13:44:47 -0500

    update for x86 compatibility

--- src/SA/arp/entry.c
+++ src/SA/arp/entry.c
@@ -6,10 +6,10 @@
 #ifdef BOF
 //DECLSPEC_IMPORT ULONG WINAPI IPHLPAPI$GetIpNetTable(PMIB_IPNETTABLE IpNetTable,PULONG SizePointer, BOOL Order);
 
-char* print_ip_from_int(unsigned int addr)
+void print_ip_from_int(unsigned int addr)
 {
         unsigned char p1, p2, p3, p4;
-		char *ipStr = (char*)intAlloc(20 * sizeof(char));
+		char ipStr[20] = {0};
 
         p1 = (addr & 0x000000FF);
         p2 = (addr & 0x0000FF00) >> 8;
@@ -17,53 +17,54 @@ char* print_ip_from_int(unsigned int addr)
         p4 = (addr & 0xFF000000) >> 24;
 
 		MSVCRT$sprintf(ipStr,"%d.%d.%d.%d", p1,p2,p3,p4);
-        return ipStr;
+		internal_printf("%-24s", ipStr);
 }
 
-char* print_MAC_from_bytes(DWORD length, BYTE* physaddr)
+void print_MAC_from_bytes(DWORD length, BYTE* physaddr)
 {
-	char *macStr = (char*)intAlloc(20 * sizeof(char));
-	for (int ii = 0; ii < length; ii++)
+	char macStr[24] = {0};
+	if(length != 6)
 	{
-		MSVCRT$sprintf(macStr+strlen(macStr),"%02X",physaddr[ii]);
-		if (ii < length-1) 
-		{
-			MSVCRT$sprintf(macStr+strlen(macStr),"%s","-");
-		}
-		
+		internal_printf("%-24s", "INVALID MAC LENGTH");
+		return;
 	}
-	return macStr;
+	MSVCRT$sprintf(macStr, "%02X-%02X-%02X-%02X-%02X-%02X",physaddr[0],physaddr[1],physaddr[2],physaddr[3],physaddr[4],physaddr[5]);
+	internal_printf("%-24s",macStr);
 }
 
 
 char* int_to_arp_type(DWORD arp_type)
 {
-	switch(arp_type)
+	if(arp_type == 1)
 	{
-		case 1:
-			return "other";
-		case 2:
-			return "invalid";
-		case 3:
-			return "dynamic";
-		case 4:
-			return "static";
-		default:
-			return "unknown";
-
+		return "other";
+	}else if(arp_type == 2)
+	{
+		return "invalid";
+	}else if(arp_type == 3)
+	{
+		return "dynamic";
+	}else if(arp_type == 4)
+	{
+		return "static";
+	}else
+	{
+		return "unknown";
 	}
+
 }
 
 
 void arp()
 {
 	ULONG ret;
-	MIB_IPNETTABLE *ipNetTableInfo = intAlloc(sizeof(PMIB_IPNETTABLE) * 32);
+	MIB_IPNETTABLE *ipNetTableInfo = NULL;
 	MIB_IPNETROW *row;
-	ULONG ipNetTableBufLen = sizeof(PMIB_IPNETTABLE) * 32;
+	ULONG ipNetTableBufLen = 0;
 	DWORD p = 0;
 	DWORD last_if_index = 0;
-
+	IPHLPAPI$GetIpNetTable(NULL, &ipNetTableBufLen, TRUE);
+	ipNetTableInfo = intAlloc(ipNetTableBufLen);
 	ret = IPHLPAPI$GetIpNetTable(ipNetTableInfo, &ipNetTableBufLen, TRUE);
 	if ((ret != NO_ERROR) && (ret != ERROR_NO_DATA))
 	{
@@ -71,7 +72,6 @@ void arp()
 		BeaconPrintf(CALLBACK_ERROR, "Could not get ipnet table info");
 		goto END;
 	}	
-
 	row = ipNetTableInfo->table;
 	for (p=0; p < ipNetTableInfo->dwNumEntries; p++)
 	{
@@ -82,21 +82,24 @@ void arp()
 			internal_printf("%-24s%-24s%-24s\n","Internet Address","Physical Address", "Type");
 		}
 		
-		internal_printf("%-24s",print_ip_from_int(row->dwAddr));
+		print_ip_from_int(row->dwAddr);
 
 		if (row->dwPhysAddrLen > 0)
 		{
-			internal_printf("%-24s",print_MAC_from_bytes(row->dwPhysAddrLen,row->bPhysAddr));
+			print_MAC_from_bytes(row->dwPhysAddrLen,row->bPhysAddr);
 		}
 		else
+		{
 			internal_printf("%-24s","");
+		}
 
 		internal_printf("%-24s\n",int_to_arp_type(row->dwType));
 		row++;
 	}
 
+
 	END:
-		p=0;	
+		if(ipNetTableInfo){ intFree(ipNetTableInfo);}	
 }
 
 VOID go( 
