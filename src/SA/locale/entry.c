4800b0fbe7a4a55814bf01bb960f5c536bb6f437
Thomas.P.Ring <thomas.p.ring@erdc.dren.mil>
2022-05-20 13:47:39 -0400

    had to lower BUFFER_SIZE to match LOCALE_NAME_MAX_LENGTH as defined in LOCALE_NAME* winnls.h and due to BOF stack constraints, removed nested if for BOF stack, checking errors

--- src/SA/locale/entry.c
+++ src/SA/locale/entry.c
@@ -3,10 +3,6 @@
 #include "base.c"
 #include "anticrash.c"
 
-DECLSPEC_IMPORT int WINAPI KERNEL32$GetLocaleInfoEx(LPCWSTR lpLocaleName, LCTYPE LCType, LPWSTR lpLCData, int cchData);
-WINBASEAPI int WINAPI KERNEL32$GetSystemDefaultLocaleName(LPCWSTR lpLocaleName, int cchLocaleName);
-DECLSPEC_IMPORT LCID WINAPI KERNEL32$LocaleNameToLCID(LPCWSTR lpName, DWORD dwFlags);
-DECLSPEC_IMPORT int WINAPI KERNEL32$GetDateFormatEx(LPCWSTR lpLocaleName, DWORD dwFlags, const SYSTEMTIME *lpData, LPCWSTR lpFormat, LPWSTR lpDateStr, int cchDate, LPCWSTR lpCalendar);
 VOID go( 
 	IN PCHAR Buffer, 
 	IN ULONG Length 
@@ -14,23 +10,48 @@ VOID go(
 {
 
 	int iResult;
-	int BUFFER_SIZE = 512;
+	#define BUFFER_SIZE 85
 	WCHAR name[BUFFER_SIZE];
 	WCHAR wcBuffer[BUFFER_SIZE];
 	WCHAR sysTime[BUFFER_SIZE];
-	WCHAR geoid[80];
+	WCHAR geoid[BUFFER_SIZE];
 	iResult = KERNEL32$GetSystemDefaultLocaleName(name, BUFFER_SIZE);
-	if(iResult > 0) {
-		iResult = KERNEL32$GetLocaleInfoEx(name, LOCALE_SENGLANGUAGE, wcBuffer, BUFFER_SIZE);
-		LCID lcid = KERNEL32$LocaleNameToLCID(name, 0);
-		iResult = KERNEL32$GetDateFormatEx(name, DATE_LONGDATE, NULL, NULL, sysTime, BUFFER_SIZE, NULL);
-		iResult = KERNEL32$GetLocaleInfoEx(name, LOCALE_SLOCALIZEDCOUNTRYNAME, geoid, BUFFER_SIZE);
-		
-		
-		BeaconPrintf(CALLBACK_OUTPUT, "Locale: %S (%S)\nLCID: %x\nDate: %S\nCountry: %S\n", wcBuffer, name, lcid, sysTime, geoid); 	
-	} else {
-		BeaconPrintf(CALLBACK_ERROR, "Error retrieving system locale information");
+	if(!iResult) {
+		DWORD error = KERNEL32$GetLastError();
+		BeaconPrintf(CALLBACK_ERROR, "Error retrieving system locale information: %ld", error);
 	}
-
+	
+	iResult = KERNEL32$GetLocaleInfoEx(name, LOCALE_SENGLANGUAGE, wcBuffer, BUFFER_SIZE);
+	if(!iResult) {
+		DWORD error = KERNEL32$GetLastError();
+		BeaconPrintf(CALLBACK_ERROR, "Error retrieving language: %ld", error);
+	}
+	
+	LCID lcid = KERNEL32$LocaleNameToLCID(name, 0);
+	if(!lcid) {
+		DWORD error = KERNEL32$GetLastError();
+		BeaconPrintf(CALLBACK_ERROR, "Error mapping Locale Name to a Locale ID: %ld", error);
+	}
+	
+	iResult = KERNEL32$GetDateFormatEx(name, DATE_LONGDATE, NULL, NULL, sysTime, BUFFER_SIZE, NULL);
+	if(!iResult) {
+		DWORD error = KERNEL32$GetLastError();
+		BeaconPrintf(CALLBACK_ERROR, "Error retrieving system date/time: %ld", error);
+	}
+	
+	iResult = KERNEL32$GetLocaleInfoEx(name, LOCALE_SLOCALIZEDCOUNTRYNAME, geoid, BUFFER_SIZE);
+	if(!iResult) {
+		DWORD error = KERNEL32$GetLastError();
+		BeaconPrintf(CALLBACK_ERROR, "Error retrieving geolocation id: %ld", error);
+	}
+	BeaconPrintf(CALLBACK_OUTPUT, "Locale: %S (%S)\nLCID: %x\nDate: %S\nCountry: %S\n", wcBuffer, name, lcid, sysTime, geoid); 	
 	return;
-};
+}
+
+#ifndef BOF
+int main() {
+	go(NULL, 0);
+	return 0;
+}
+#endif
+
